9be6f21d95f838b6a8f4f60e73e5cb9a6d7d6c91
lacgialac <nguyengiahuylac@gmail.com>
2026-05-25 14:47:22 +0700

    Add files via upload

---
+++ bt3.py
@@ -0,0 +1,10 @@
+student_name = input("Nhap ten sinh vien: ")
+student_age = input("Nhap tuoi: ")
+student_class = input("Nhap lop: ")
+
+print("\nThong tin sinh vien:")
+print(f"Ten sinh vien: {student_name}")
+print(f"Tuoi: {student_age}")
+print(f"Lop hoc: {student_class}")
+
+print(f"\nChao mung {student_name} den voi mon Python!")
